fb9d78f27a08460dcea392d8781ba89edd6a2043
RLumSK <sebastian.kreutzer@aber.ac.uk>
2021-11-11 19:04:05 +0000

    add_Population(): make sure also this functions knows about the attributes

--- R/add_Population.R
+++ R/add_Population.R
@@ -14,10 +14,12 @@
 #' @examples
 #'
 #' ## create simple true age-depth-relationship
-#' book_1 <- get_RuleBook(book = "empty")
+#' book_1 <- get_RuleBook()
 #' 
-#' book_2 <- add_Population(book = book_1, 
-#'                          populations = 1)
+#' book_2 <- add_Population(
+#'  book = book_1,
+#'  populations = 1)
+#'  
 #' @md                            
 #' @export add_Population
 add_Population <- function(
@@ -25,6 +27,16 @@ add_Population <- function(
   populations = 1
 ) {
   
+# Input checks ------------------------------------------------------------
+  ## check input
+  if (!all(c("sandbox", "book") %in% attributes(book)[c("package", "medium")]))
+    stop("[add_Population()] 'book' is not an object created by sandbox!", call. = FALSE)
+
+# Process -----------------------------------------------------------------
+  ##maintain all attributes except names
+  attr_default <- attributes(book)[names(attributes(book)) != "names"]
+  
+ 
   ## remove first book entry
   book_body <- book
   book_body[[1]] <- NULL
@@ -33,8 +45,7 @@ add_Population <- function(
   book_body <- lapply(X = book_body, FUN = function(book_body, populations) {
     
     ## check if rule is of group specific
-    if(book_body$group == "specific") {
-      
+    if (book_body$group == "specific") {
       ## extract base name of rule
       name_base <- strsplit(x = names(book_body)[2], 
                             split = "_")[[1]]
@@ -43,8 +54,7 @@ add_Population <- function(
                          collapse = "_")
       
       ## append all populations
-      for(i in 1:populations) {
-        
+      for (i in 1:populations) {
         ## get existing number of populations
         n <- length(book_body)
         
@@ -63,11 +73,17 @@ add_Population <- function(
     
   }, populations)
   
+
   ## amalgamate book parts
   book_new <- c(book[1], 
                 book_body)
+  
+ 
+  ## restore attributes
+  attributes(book_new) <- attr_default
   
   ## return output
   return(book_new)
 
 }
+
