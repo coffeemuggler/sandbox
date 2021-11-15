08c1e430a0e0b5c055bd3c109bb06261f0b2ad1e
RLumSK <sebastian.kreutzer@aber.ac.uk>
2021-11-15 18:02:35 +0000

    measure_SAR_OSL(): + major code refactoring to ensure that the function works in the future and is more robust + add example #3 + Update documentation + Add self-call + Add incoming checks

--- R/measure_SAR_OSL.R
+++ R/measure_SAR_OSL.R
@@ -10,28 +10,45 @@
 #' The function uses the package [RLumModel-package] to perform the simulation of the 
 #' photon count curves. 
 #' 
-#' @param aliquot [data.frame], a set of grains that are assigned to an 
+#' @param aliquot [data.frame] or a [list] of it, a set of grains that are assigned to an 
 #' aliquot (sample subset used for measurement), i.e., the result of 
-#' [prepare_Aliquot].
+#' [prepare_Aliquot]. 
 #' 
-#' @param sequence [list], Definition of the SAR protocol.
+#' @param sequence [list], definition of the SAR protocol.
 #' 
 #' @param dose_rate [numeric] value, Dose rate of the luminescence 
-#' reader, in Gy.
+#' reader, in Gy/s.
 #' 
 #' @return [Luminescence::RLum.Analysis-class] object. Equivalent of the import result for 
 #' a real world measurement file. This object can be evaluated by functions 
 #' of the package [Luminescence-package].
 #' 
-#' @author Michael Dietze, GFZ Potsdam (Germany)
+#' @author Michael Dietze, GFZ Potsdam (Germany), 
+#' Sebastian Kreutzer, Geography & Earth Sciences, Aberystwyth University (United Kingdom)
 #' 
 #' @examples
 #' 
-#' \dontrun{
+#' ## load example data set
+#' data(sample_osl_aliquots, envir = environment())
 #' 
-#' ## create dummy data set
+#' sequence <- list(
+#'   RegDose = c(0, 1, 2, 5, 10, 0, 1),
+#'   TestDose = 2,
+#'   PH = 220,
+#'   CH = 200,
+#'   OSL_temp = 125,
+#'   OSL_duration = 70)
+#'
+#' ## reduce number of 
+#' ## grains to two
+#' sample_osl_aliquots$aliquot_1 <- 
+#' sample_osl_aliquots$aliquot_1[1:2,]
 #' 
-#' }
+#' ## or measure all aliquots in a row
+#' sar_all <- measure_SAR_OSL(
+#'  aliquot = sample_osl_aliquots,
+#'  sequence = sequence,
+#'  dose_rate = 0.1)
 #' 
 #' @md 
 #' @export measure_SAR_OSL
@@ -41,44 +58,44 @@ measure_SAR_OSL <- function(
   dose_rate = 0.1
 ) {
   
+  # Self-call ---------------------------------------------------------------
+  if (is(aliquot, "list"))
+    return(lapply(aliquot, measure_SAR_OSL, sequence = sequence, dose_rate = dose_rate))
+
+  # Check incoming ----------------------------------------------------------
+  if (is.null(attributes(aliquot)$package) || attributes(aliquot)$package != "sandbox")
+    stop("[measure_SAR_OSL()] the input for aliquot is not an object created by 'sandbox'!", 
+         call. = FALSE)
+  
+  if (!is(aliquot, "data.frame"))
+    stop("[measure_SAR_OSL()] the input for aliquot is not of type data.frame!", 
+         call. = FALSE)
+  
+
   ## PART 1 - separate OSL components -----------------------------------------
+  ## get column IDs for further processing
+  col_names <- colnames(aliquot)
+  col_names <- col_names[grepl("osl_", col_names)]
   
-  ## collect model parameters
-  N <- colMeans(x = aliquot[,grepl(x = colnames(aliquot), 
-                                   pattern = "osl_N")])
+  ## calculate column means (for the variables for interest)
+  col_means <- colMeans(aliquot[,col_names])
+
+  ## get unique variable names
+  var_names <- unique(unlist(regmatches(col_names, regexec(
+    "(?<=osl\\_)\\D+"
+  , col_names, perl = TRUE))))
   
-  En <- colMeans(x = aliquot[,grepl(x = colnames(aliquot), 
-                                    pattern = "osl_E") & 
-                               !grepl(x = colnames(aliquot), 
-                                      pattern = "osl_E_th")])
+  ## fetch relevant values from the table and write into new variable
+  parameters <- lapply(var_names, function(v){
+    col_means[grepl(paste0("osl_", v,"\\d?"), col_names, perl = TRUE)]
+    
+  })
+
+  ## add parameter names to the list
+  names(parameters) <- var_names
   
-  s <- colMeans(x = aliquot[,grepl(x = colnames(aliquot), 
-                                   pattern = "osl_s")])
-  
-  A <- colMeans(x = aliquot[,grepl(x = colnames(aliquot), 
-                                   pattern = "osl_A")])
-  
-  B <- colMeans(x = aliquot[,grepl(x = colnames(aliquot), 
-                                   pattern = "osl_B")])
-  
-  Th <- colMeans(x = aliquot[,grepl(x = colnames(aliquot), 
-                                    pattern = "osl_Th")])
-  
-  E_th <- colMeans(x = aliquot[,grepl(x = colnames(aliquot), 
-                                      pattern = "osl_E_th")])
-  
-  R <- mean(x = aliquot[,grepl(x = colnames(aliquot), 
-                               pattern = "osl_R")])
-  
-  parameters <- list(N = as.numeric(N),
-                     E = as.numeric(En),
-                     s = as.numeric(s),
-                     A = as.numeric(A),
-                     B = as.numeric(B),
-                     Th = as.numeric(Th),
-                     E_th = as.numeric(E_th),
-                     model = "customized",
-                     R = as.numeric(R))
+  ## add model information
+  parameters <- c(parameters, model = "customized")
   
   ## calculate mean burial dose
   burial_dose <- mean(aliquot$osl_doserate * aliquot$age)
@@ -88,7 +105,7 @@ measure_SAR_OSL <- function(
   
   ## PART 2 - model luminescence ----------------------------------------------
   osl_model <- RLumModel::model_LuminescenceSignals(
-    model = "customized",
+    model = parameters$model,
     sequence = sequence, 
     lab.dose_rate = dose_rate,
     own_parameters = parameters,
