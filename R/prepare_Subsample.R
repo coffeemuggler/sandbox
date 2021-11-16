610159a27379e9a47dee86f6ad3a33895ec476ab
RLumSK <sebastian.kreutzer@aber.ac.uk>
2021-11-16 18:38:24 +0000

    prepare_Subsample(): + brief code review + minor code polish + code update with incomings + add missing example + add tests

--- R/prepare_Subsample.R
+++ R/prepare_Subsample.R
@@ -29,6 +29,9 @@
 #' @examples
 #' ## load example data set
 #' data(sample)
+#' 
+#' ## create 10 subsamples
+#' prepare_Subsample(sample, 10) 
 #'
 #' @md
 #' @export prepare_Subsample
@@ -39,39 +42,46 @@ prepare_Subsample <- function(
   weight
 ) {
   
+# Check incoming ----------------------------------------------------------
+  if (is.null(attributes(sample)$package) || attributes(sample)$package != "sandbox")
+    stop("[prepare_Subsample()] the input for sample is not an object created by 'sandbox'!", 
+         call. = FALSE)
+  
+  if (!is(sample, "data.frame"))
+    stop("[prepare_Subsample()] the input for sample is not of type data.frame!", 
+         call. = FALSE)  
+  
   ## check if only one subsampling criterion is given
-  if(sum(c(!missing(number), !missing(volume), !missing(weight))) > 1) {
+  if (sum(c(!missing(number), !missing(volume), !missing(weight))) > 1) {
     stop("[prepare_Subsample()] Only one criterion for subsampling is allowed!", 
          call. = FALSE)
   }
   
+# Sub sample --------------------------------------------------------------
   ## option 1, subsampling by number of grains
-  if(missing(number) == FALSE) {
+  if (!missing(number)) {
     
     ## get number of grains in sample
     n_grains <- length(sample$grains)
     
     ## get number of grains per sample
-    n_sub <- floor(n_grains / number)
+    n_sub <- floor(n_grains / number[1])
     
     ## create subsampling vector
     i_sub <- seq(from = 1, to = n_grains, by = n_sub)
     
     ## create and fill slice object with grain depths  
-    sample_sub <- vector(mode = "list", 
-                         length = length(i_sub) - 1)
+    sample_sub <- vector(mode = "list", length = length(i_sub) - 1)
     
-    for(i in 1:length(sample_sub)) {
-      
+    for (i in 1:length(sample_sub)) 
       sample_sub[[i]] <- sample[(i_sub[i]):(i_sub[i + 1] - 1),]
-    }
+    
   }
   
   ## option 2, subsampling by volume
-  if(!missing(volume)) {
-    
+  if (!missing(volume)) {
     ## get grain radius
-    r_grains <- EMMAgeo::convert.units(phi = sample$grainsize) / (2 * 10^6)
+    r_grains <- convert_units(phi = sample$grainsize) / (2 * 1e+06)
     
     ## get grain volumes
     v_grains <- cumsum(4 / 3 * pi * r_grains^3 * 1/sample$packing)
@@ -82,16 +92,14 @@ prepare_Subsample <- function(
     ## assign grains to subsamples
     sample_sub <- lapply(X = v_subset,
                          FUN = function(x, v_grains, volume, sample) {
-                           
                            sample[v_grains > x - volume & v_grains <= x,]
                          }, v_grains, volume,sample)
   }
   
   ## option 2, subsampling by weight
-  if(!missing(weight)) {
-    
+  if (!missing(weight)) {
     ## get grain radius
-    r_grains <- EMMAgeo::convert.units(phi = sample$grainsize) / (2 * 10^6)
+    r_grains <- convert_units(phi = sample$grainsize) / (2 * 1e+06)
     
     ## get grain volumes
     v_grains <- 4 / 3 * pi * r_grains^3
@@ -105,10 +113,16 @@ prepare_Subsample <- function(
     ## assign grains to subsamples
     sample_sub <- lapply(X = m_subset,
                          FUN = function(x, m_grains, weight, sample) {
-                           
                            sample[m_grains > x - weight & m_grains <= x,]
                          }, m_grains, weight, sample)
   }
+  
+# Create output -----------------------------------------------------------
+  ## set list names
+  names(sample_sub) <- paste0("subsample_", 1:length(sample_sub))
+  
+  ## set package attributes
+  attr(sample_sub, "package") <- "sandbox"
   
   ## return output
   return(sample_sub)
